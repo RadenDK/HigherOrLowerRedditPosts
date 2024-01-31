d75b46ae7c24fe7e7878d3f4bf22f00d347417ca
RadenDK <128796441+RasmusJensenUCN@users.noreply.github.com>
2024-01-31 09:04:45 +0100

    Update package_builder.py

--- python-scripts/reddit-posts-updater/package_builder.py
+++ python-scripts/reddit-posts-updater/package_builder.py
@@ -1,36 +1,47 @@
-import subprocess
+import os
 import platform
 import shutil
+import subprocess
+
 
 def activate_virtual_environment():
     activate_command = "venv\\Scripts\\activate" if platform.system() == "Windows" else "source venv/bin/activate"
     subprocess.run(activate_command, shell=True)
 
-def deactivate_virtual_environment():
-    subprocess.run("deactivate", shell=True)
 
 def capture_freeze_result():
     freeze_result = subprocess.run(["pip", "freeze"], stdout=subprocess.PIPE, text=True)
     return freeze_result.stdout
 
+
 def write_requirements_to_file(requirements_file_name, freeze_result):
+    # Extract the directory path from the file name
+    directory = os.path.dirname(requirements_file_name)
+
+    # Create the directory structure if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     with open(requirements_file_name, 'w') as requirements_file:
         requirements_file.write(freeze_result)
+
 
 def install_requirements(target_directory, requirements_file_name):
     subprocess.run(["pip", "install", "-r", requirements_file_name, "-t", target_directory, "--upgrade"])
 
+
 def copy_source_file(source_file, target_directory):
     shutil.copy(source_file, target_directory)
+
 
 def create_zip_archive(target_directory, zip_file_name):
     shutil.make_archive(zip_file_name, 'zip', target_directory)
 
+
 def main():
     # Path to the directory where you want to install the dependencies
+    source_file = "lambda_function.py"
     target_directory = "lambda_build/package"
     requirements_file_name = "lambda_build/requirements.txt"
-    source_file = "lambda_function.py"
     zip_file_name = "lambda_build/deploy_package.zip"
 
     # Activate the virtual environment
@@ -38,9 +49,6 @@ def main():
 
     # Run pip freeze and capture the output
     freeze_result = capture_freeze_result()
-
-    # Deactivate the virtual environment
-    deactivate_virtual_environment()
 
     # Write the output to requirements.txt
     write_requirements_to_file(requirements_file_name, freeze_result)
@@ -53,6 +61,7 @@ def main():
 
     # Create a zip archive
     create_zip_archive(target_directory, zip_file_name)
+
 
 if __name__ == "__main__":
     main()
