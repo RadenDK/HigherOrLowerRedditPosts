89cf24193bfb61bd1c1bc9d16e9111f53fabfad8
RadenDK <128796441+RasmusJensenUCN@users.noreply.github.com>
2024-01-31 10:41:34 +0100

    Update lambda_function.py

--- python-scripts/reddit-posts-updater/lambda_function.py
+++ python-scripts/reddit-posts-updater/lambda_function.py
@@ -7,8 +7,6 @@ import praw
 
 def lambda_handler(event, context):
 
-    print("CI/CD pipeline worked at least")
-
     reddit_accessor = RedditAccessor()
 
     reddit_posts = reddit_accessor.retrieve_todays_reddit_posts()
