554fdde30a967e82ebb45571e09e7123f9e8687f
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-02-18 11:04:22 +0100

    created score component

---
+++ website/src/components/ScoreComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/ScoreComponentStyle.css';
+
+
+const Score = (props) => {
+    const { score } = props;
+
+    return (
+        <div className='score'>
+            <h1>{score}</h1>
+        </div>
+    );
+};
+
+export default Score;
