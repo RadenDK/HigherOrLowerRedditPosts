f90d4e72d3531017e9327bed55ede089f1b454e0
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-02-29 16:42:33 +0100

    Update Program.cs

--- api-service/api-service/Program.cs
+++ api-service/api-service/Program.cs
@@ -1,22 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+
+// Configure Kestrel to use a port defined in the environment variables (for Heroku)
+string port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseKestrel().UseUrls("http://*:" + port);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(policy =>
+    policy.WithOrigins("http://localhost:3000", "https://localhost:3000", "http://localhost:3000/GamePage", "https://localhost:3000/GamePage",
+    "http://higher-or-lower-clone.s3-website.eu-north-1.amazonaws.com", "https://higher-or-lower-clone.s3-website.eu-north-1.amazonaws.com",
+    "http://higher-or-lower-clone.s3-website.eu-north-1.amazonaws.com/GamePage", "https://higher-or-lower-clone.s3-website.eu-north-1.amazonaws.com/GamePage"
+    )
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 
 app.UseAuthorization();
 
