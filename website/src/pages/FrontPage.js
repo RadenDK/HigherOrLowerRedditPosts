eaf7233c810f3a7901ed5198b9a85b97856f6173
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-02-29 17:32:28 +0100

    Changed front page a bit

--- website/src/pages/FrontPage.js
+++ website/src/pages/FrontPage.js
@@ -7,6 +7,7 @@ function FrontPage({ startGame }) {
     <div className="start-menu-background">
       <div className="start-menu-content">
         <h1>Higher or Lower Reddit AMITA </h1>
+        <h3>An attempt to clone <a href="http://www.higherlowergame.com/" target="_blank">higherlowergame</a> but with reddit daily AITA post scores</h3>
         <div className="button-container">
           <Link to="./GamePage" className="start-game-button btn btn-info btn-lg">
             Start Playing
